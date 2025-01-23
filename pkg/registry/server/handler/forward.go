1947f96c8d1f343168b588ac88d52e83fd71155f
苓诣 <2097434924@qq.com>
2025-01-23 20:31:23 +0800

    upload 与 download 接口重构

--- pkg/registry/server/handler/forward.go
+++ pkg/registry/server/handler/forward.go
@@ -9,7 +9,7 @@ import (
 	"path/filepath"
 )
 
-// HandlePostAndForward 处理 POST 请求并实时转发数据
+// ForwardHandler 处理 POST 请求并实时转发数据
 // TODO:
 
 type ForwardHandler struct {
@@ -25,6 +25,7 @@ func (d *ForwardHandler) GetHandler() func(w http.ResponseWriter, r *http.Reques
 	return d.Handler
 }
 
+// NewForwardHandler 创建一个新的 ForwardHandler 实例
 func NewForwardHandler(dataPath string, fileMapping *data.FileMapping) *ForwardHandler {
 	dh := &ForwardHandler{
 		DataPath:    dataPath,
